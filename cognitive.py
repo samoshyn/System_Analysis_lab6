2fa0763b629e3db9322560e0aec75d6f81deb445
Andriy Samoshyn <62052848+samoshyn@users.noreply.github.com>
2022-04-14 18:03:33 +0300

    Add files via upload

---
+++ cognitive.py
@@ -0,0 +1,105 @@
+from typing import Any, Dict, List, NoReturn, Optional
+
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+
+
+def cycle_str(x: List[int]) -> str:
+    return " - ".join(str(y) for y in x)
+
+
+class CognitiveModel:
+    def __init__(self, adjacency_matrix: np.ndarray, nodes_names: Optional[Dict[int, Any]] = None):
+        self.graph = nx.DiGraph(incoming_graph_data=adjacency_matrix)
+        self.nodes_names = (
+            nodes_names if nodes_names is not None else {key: f"V{key + 1}" for key in range(len(adjacency_matrix))}
+        )
+        if nodes_names:
+            nx.set_node_attributes(self.graph, "name", nodes_names)
+
+    @property
+    def adjacency_matrix(self) -> np.ndarray:
+        return nx.adjacency_matrix(self.graph).todense()
+
+    @property
+    def graph_weights(self) -> Dict[Any, Any]:
+        return nx.get_edge_attributes(self.graph, "weight")
+
+    @staticmethod
+    def get_spectral_radius(matrix: np.ndarray) -> float:
+        return np.max(np.absolute(np.linalg.eigvals(matrix)))
+
+    def check_perturbation_stability(self) -> bool:
+        if self.get_spectral_radius(self.adjacency_matrix) <= 1:
+            return True
+        else:
+            return False
+
+    def check_numerical_stability(self) -> bool:
+        if self.get_spectral_radius(self.adjacency_matrix) < 1:
+            return True
+        else:
+            return False
+
+    def is_even(self, cycle: List[int]) -> bool:
+        negative_count = np.count_nonzero(
+            np.less([self.graph_weights[edge] for edge in nx.find_cycle(self.graph, cycle)], 0)
+        )
+        return not negative_count & 0x1
+
+    def check_structural_stability(self) -> List[List[int]]:
+        return [cycle for cycle in nx.simple_cycles(self.graph) if self.is_even(cycle)]
+
+    def calculate_eigenvalues(self) -> np.ndarray:
+        return np.linalg.eigvals(self.adjacency_matrix)
+
+    def draw_graph(self) -> NoReturn:
+        # rcParams["figure.figsize"] = 10, 8
+        fig, ax = plt.subplots()
+        colors = ["g" if self.graph_weights[edge] > 0 else "r" for edge in self.graph.edges()]
+        nx.draw_networkx(
+            self.graph,
+            pos=nx.circular_layout(self.graph),
+            arrows=True,
+            with_labels=True,
+            edge_color=colors,
+            font_size=10,
+            font_color="#ffffff",
+            node_color="k",
+            labels=self.nodes_names,
+            node_size=2400,
+            ax=ax
+        )
+        ax.set_title("Когнітивна карта")
+        return fig
+
+    def impulse_model(self, t: int = 5, q: Optional[np.ndarray] = None) -> NoReturn:
+        # rcParams["figure.figsize"] = 7, 5
+        x_0 = np.zeros((self.adjacency_matrix.shape[0], 1))
+        init_q = x_0.copy()
+        x_list = [x_0, x_0]
+        if q is None:
+            q = init_q.copy()
+            q[1] = 1
+        else:
+            q = np.array(q).reshape(-1, 1)
+        save_q = q.copy()
+
+        for _ in range(t):
+            x_next = x_list[-1] + np.dot(self.adjacency_matrix, (x_list[-1] - x_list[-2])) + q
+            x_list.append(x_next)
+            q = init_q.copy()
+        x_plot = np.array(x_list[1:])
+        x_plot = x_plot.reshape(x_plot.shape[:2])
+
+        fig, ax = plt.subplots()
+
+        for index in range(x_plot.shape[1]):
+            ax.plot(range(t + 1), x_plot[:, index], label=f"V{index + 1}")
+        ax.set_title(f"Графік імпульсних процесів у вершинах \n q = {save_q.reshape(-1, )}")
+        ax.set_xlabel("Кроки")
+        ax.set_ylabel("x(t)")
+        ax.legend()
+
+        return fig
---
+++ first_app.py
@@ -0,0 +1,126 @@
+import re
+import time
+
+import pandas as pd
+import streamlit as st
+
+from cognitive import *
+
+st.set_page_config(layout="wide")
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+hide_footer_style = """
+    <style>
+    .reportview-container .main footer {visibility: hidden;}    
+    """
+st.markdown(hide_footer_style, unsafe_allow_html=True)
+
+
+def get_df(file):
+    extension = file.name.split('.')[1]
+    if extension.upper() == 'CSV':
+        df = pd.read_csv(file)
+    elif extension.upper() == 'XLSX':
+        df = pd.read_excel(file, engine='openpyxl')
+    elif extension.upper() == 'TXT':
+        df = pd.read_csv(file, sep=" ", header=None)
+    else:
+        return pd.DataFrame([])
+    return df
+
+
+def run_iteration(df, q, t, i, to_header, to_columns, to_impulse):
+    a = df.to_numpy()
+    to_remove = '[]'
+    to_header.subheader(f'Ітерація {i}')
+    cognitive_model = CognitiveModel(a)
+    calculate_eigenvalues = cognitive_model.calculate_eigenvalues()
+    get_spectral_radius = cognitive_model.get_spectral_radius(cognitive_model.adjacency_matrix)
+    check_perturbation_stability = cognitive_model.check_perturbation_stability()
+    check_numerical_stability = cognitive_model.check_numerical_stability()
+    check_structural_stability = cognitive_model.check_structural_stability()
+
+    col1, col2, col3 = to_columns.columns(3)
+
+    col1.subheader('Власні числа')
+    for x in calculate_eigenvalues:
+        col1.write(str(x))
+    col1.write(f'**Максимум:** {get_spectral_radius}')
+
+    col2.subheader('Властивості')
+    col2.write(f'**Стійкість за збуренням:** {"Так" if check_perturbation_stability else "Ні"}')
+    col2.write(f'**Стійкість за значенням:** {"Так" if check_numerical_stability else "Ні"}')
+    col2.write(f'**Структурна стійкість:** {"Так" if not check_structural_stability else "Ні"}')
+
+    col3.subheader('Отримані цикли')
+    res = [f'[{cycle_str(x)}]' for x in check_structural_stability][::-1]
+    col3.dataframe(res)
+    if len(res):
+        col3.write(f'**Рекомендовано для видалення:** {res[0]}')
+        to_remove = res[0]  # to_select.selectbox("Виберіть для видалення", res, 0, key=i)
+    else:
+        col3.write(f'**Видаляти нічого**')
+
+    fig_col1, fig_col2 = to_impulse.columns(2)
+
+    fig1 = cognitive_model.impulse_model(t=t, q=q)
+    fig_col1.pyplot(fig1)
+
+    fig2 = cognitive_model.draw_graph()
+    fig_col2.pyplot(fig2)
+
+    return list(map(int, re.findall(r'\d+', to_remove)))
+
+
+def main():
+    st.header('Налаштування')
+    st.info("Завантажте матрицю")
+    uploaded_file = st.file_uploader("Завантажити матрицю")
+    if uploaded_file is not None:
+        df = get_df(uploaded_file)
+        q_input = st.text_input("Ввести вектор імпульсу (через пробіл)", ' '.join(['0'] * len(df)))
+        if len(q_input.split()) == len(df):
+            t = st.number_input("Ввести T", min_value=0, value=5)
+            go = st.button('Виконати')
+            if go:
+
+                step = 0
+
+                q = np.array(list(map(float, q_input.split())))
+                df.columns = range(len(df))
+                df.index = range(len(df))
+
+                to_header = st.empty()
+                to_table = st.empty()
+                to_columns = st.empty()
+                to_impulse = st.empty()
+
+                to_table.table(df)
+                st.subheader("Видалені зв'язки")
+
+                while True:
+                    to_remove = run_iteration(df, q, t, step, to_header, to_columns,
+                                              to_impulse)
+                    for i in range(len(to_remove) - 1):
+                        df.iloc[to_remove[i], to_remove[i + 1]] = 0
+
+                    to_table.table(df)
+                    st.write(f"**Ітерація** {step}: {' - '.join([str(x) for x in to_remove])}")
+                    time.sleep(1.5)
+                    step += 1
+
+                    if not len(to_remove):
+                        break
+        else:
+            st.error('Введено вектор неправильної розмірності')
+
+
+if __name__ == "__main__":
+    main()
